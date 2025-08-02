5c44205d93fe134eb72229c20d89c70463267eb7
Hamza Boussetta <hbousset@c1r2p3.1337.ma>
2025-08-02 01:52:27 +0100

    first commit

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 00:32:12 by hbousset          #+#    #+#             */
+/*   Updated: 2025/08/02 01:44:58 by hbousset         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <ctype.h>
+
+int	main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		int	i = 1;
+		while (i < ac)
+		{
+			int	j = 0;
+			while (av[i][j])
+			{
+				char c = av[i][j];
+				std::cout << (char)toupper(c);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
+}
+
